fe29aef85381d76786ed7023f96b62946a71bd5d
ubuntu <teho@ubuntu.(none)>
2013-06-26 10:23:56 +0900

    add page and modi site title

--- config/initializers/refinery/core.rb
+++ config/initializers/refinery/core.rb
@@ -15,7 +15,7 @@ Refinery::Core.configure do |config|
   # config.base_cache_key = :refinery
 
   # Site name
-   config.site_name = "RUBY TEST"
+   config.site_name = "Rubyビジネス推進協議会"
 
   # This activates Google Analytics tracking within your website. If this
   # config is left blank or set to UA-xxxxxx-x then no remote calls to
